73d45c0ed6b2515ba736f4fb14231552c874b900
DOTRUONGBACH <vuabongdapro@gmail.com>
2023-01-02 21:59:34 +0700

    ok

--- src/main/java/com/example/case_module3/services/HatService.java
+++ src/main/java/com/example/case_module3/services/HatService.java
@@ -11,5 +11,13 @@ public void Add(Hat hat){
 HatDao.getInstance().insert(hat);
 hats = HatDao.getInstance().selectAll();
 }
+public void Delete(int id){
+    HatDao.getInstance().delete(id);
+   hats = HatDao.getInstance().selectAll();
+}
+public void Edit(){
+    HatDao.getInstance().update(HatDao.getInstance().selectById());
+    hats = HatDao.getInstance().selectAll();
+}
 
 }
